106fbb3b707a433b5ef8518a95bbc4656f48743d
Beaps <beapsant@gmail.com>
2019-04-03 21:29:21 +0100

    Listen form

--- js/app.js
+++ js/app.js
@@ -2,3 +2,15 @@ const $form = document.querySelector('.form-task');
 const $btn = document.querySelector('.btn');
 let $task = document.querySelector('#task');
 //const tasks = [];
+
+
+$form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if ($task.value.length > 0) {
+    //tasks.push($task.value); 
+    renderList($task);
+  }
+  // Reset text
+  $task.value = '';
+});
