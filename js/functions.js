300dec55ae9bc61094b0a3373508fd1be0e12ef6
Beaps <beapsant@gmail.com>
2019-05-16 21:46:30 +0100

    Style to-do list

--- js/functions.js
+++ js/functions.js
@@ -5,27 +5,27 @@ function task(tasks) {
 
 // Render an individual task
 function renderList(task) {
-  const $ul = document.querySelector('.list-container');
+  const $ul = document.querySelector(".list-container");
   $ul.appendChild(generateElementsDOM(task));
 }
 
 // Get the DOM elements for an individual task
 function generateElementsDOM(task) {
-  const $li = document.createElement('li');
-  const $label = document.createElement('label');
-  const $checkbox = document.createElement('input');
-  const $taskText = document.createElement('p');
-  const $removeButton = document.createElement('button');
+  const $li = document.createElement("li");
+  const $label = document.createElement("label");
+  const $checkbox = document.createElement("input");
+  const $taskText = document.createElement("p");
+  const $removeButton = document.createElement("button");
 
   // Setup checkbox
-  $checkbox.setAttribute('type', 'checkbox');
-  $checkbox.addEventListener('click', (e) => {
-    $taskText.classList.toggle('list-item__text--line-through');
+  $checkbox.setAttribute("type", "checkbox");
+  $checkbox.addEventListener("click", e => {
+    $taskText.classList.toggle("list-item__text--line-through");
 
     if (task.done) {
-      $checkbox.setAttribute('checked', 'checked');
+      $checkbox.setAttribute("checked", "checked");
     } else {
-      $checkbox.removeAttribute('checked');
+      $checkbox.removeAttribute("checked");
     }
 
     task.done = $checkbox.checked;
@@ -34,26 +34,27 @@ function generateElementsDOM(task) {
 
   // If there are already values in the localStorage
   if (task.done) {
-    $checkbox.setAttribute('checked', 'checked');
-    $taskText.classList.add('list-item__text--line-through');
+    $checkbox.setAttribute("checked", "checked");
+    $taskText.classList.add("list-item__text--line-through");
   } else {
-    $checkbox.removeAttribute('checked');
-    $taskText.classList.remove('list-item__text--line-through');
+    $checkbox.removeAttribute("checked");
+    $taskText.classList.remove("list-item__text--line-through");
   }
 
   $label.appendChild($checkbox);
 
   // Setup task text
   $taskText.textContent = task.text;
-  $taskText.classList.add('list-item__text');
+  $taskText.classList.add("list-item__text");
   $label.appendChild($taskText);
 
   // Setup remove button
-  $removeButton.textContent = 'Borrar';
-  $removeButton.addEventListener('click', e => {
+  $removeButton.textContent = "Borrar";
+  $removeButton.addEventListener("click", e => {
     e.target.parentNode.remove();
-    removeItemFromLocalStorage(task)
+    removeItemFromLocalStorage(task);
   });
+  $removeButton.classList.add("btn__remove");
 
   // Setup li
   $li.appendChild($label);
@@ -64,12 +65,12 @@ function generateElementsDOM(task) {
 
 // Save tasks to localStorage
 function saveTasks(tasks) {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Fetch existing tasks from localStorage
 function getSavedTasks() {
-  const tasksJSON = localStorage.getItem('tasks');
+  const tasksJSON = localStorage.getItem("tasks");
 
   try {
     return tasksJSON ? JSON.parse(tasksJSON) : [];
@@ -83,5 +84,5 @@ function removeItemFromLocalStorage(task) {
   let index = tasks.indexOf(task);
   tasks.splice(index, 1);
 
-  saveTasks(tasks)
+  saveTasks(tasks);
 }
